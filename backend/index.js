1980e470ab8a9341b9d23c218b680997d720d90a
Javier Maiz <javi13doce@gmail.com>
2023-11-22 00:22:20 +0100

    creacion servidor y routes

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
 // Entrada al servidor
+
+const express = require('express');  
+const app = express();  
+
+require('dotenv').config(); 
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => { 
+    console.log(`Escuchando el puerto ${PORT}`);
+}); 
